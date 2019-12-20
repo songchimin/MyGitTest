0fca40e0a0296eb561adb7ef3fd6c0d10894c161
songcm <vzcxds@gmail.com>
2019-12-20 18:33:00 +0900

    dd

---
+++ MYTry2/app/src/main/java/com/study/android/mytry/mypage/setup/ComplaintContent.java
@@ -0,0 +1,15 @@
+package com.study.android.mytry.mypage.setup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.study.android.mytry.R;
+
+public class ComplaintContent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mypage_complaint_content);
+    }
+}

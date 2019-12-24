7509d7197b4993737b6d040ea1a8ba805001a5ca
songcm <vzcxds@gmail.com>
2019-12-24 19:48:59 +0900

    dasdsad

--- ZSong/src/main/java/com/study/springboot/dto/MapDto.java
+++
@@ -1,9 +0,0 @@
-package com.study.springboot.dto;
-
-import lombok.Data;
-
-@Data
-public class MapDto {
-   private double latitude;
-   private double longitude;
-}
--- ZSong/src/main/java/com/study/springboot/dto/filterDto.java
+++
@@ -1,8 +0,0 @@
-package com.study.springboot.dto;
-
-import lombok.Data;
-
-@Data
-public class filterDto {
-	private String interest_context;
-}

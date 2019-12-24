f74ffa3d9c9322ca95dba91ae462b7c9f3287c3f
songcm <vzcxds@gmail.com>
2019-12-24 10:59:05 +0900

    ss

--- ZSong/src/main/java/com/study/springboot/dao/LoginDao.java
+++
@@ -1,8 +0,0 @@
-package com.study.springboot.dao;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface LoginDao {
-	public String UserJoin(String id, String pw, String name, String nicname);
-}
